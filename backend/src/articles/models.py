81f2c30427456702942b2763f8011ef9281b99f6
jaindivya2003 <dj92dodo@gmail.com>
2019-10-12 21:33:14 +0530

    files needed for db migrations

---
+++ backend/src/articles/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+
+    def _str_(self):
+     return self.title
